e56a3dd62db17708e735969f3363f29eaf942f7b
AnthonyCustodia <anthonycustodia@gmail.com>
2026-07-01 19:56:49 -0300

    Ajustando classes

---
+++ src/biblioteca/gerenciador/Emprestimo.java
@@ -0,0 +1,8 @@
+package biblioteca.gerenciador;
+
+public interface Emprestimo {
+
+    public void emprestar();
+    public void devolver();
+
+}
--- src/biblioteca/gerenciador/Gerenciador.java
+++ src/biblioteca/gerenciador/Gerenciador.java
@@ -1,4 +1,27 @@
 package biblioteca.gerenciador;
 
-public class Gerenciador {
+import biblioteca.livros.Livro;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Gerenciador implements Emprestimo {
+
+    List<Livro> prateleira = new ArrayList<>();
+    private Date dataEmprestimo;
+
+    public void adicionar(String titulo, String autor, int anoPubli) {
+        prateleira.add(titulo, autor, anoPubli);
+    }
+
+    @Override
+    public void emprestar() {
+
+    }
+
+    @Override
+    public void devolver() {
+
+    }
 }
